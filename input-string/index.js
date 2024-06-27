5e25833c5160eed68275b47d068ec89318c284c9
Mandeep-Singh <mandeep.dev1309@gmail.com>
2024-06-27 14:35:43 +0530

    7

--- input-string/index.js
+++ input-string/index.js
@@ -1,22 +1,7 @@
-var mycheckbox = document.getElementById("mycheckbox");
-var myp = document.getElementById("myp");
-var my1 = document.getElementById("1");
-var my2 = document.getElementById("2");
-var my3 = document.getElementById("3");
-var mysubmit = document.getElementById("mysubmit");
+var username = document.getElementById("username");
+var gmail = document.getElementById("gmail");
+var text = document.getElementById("text");
 
-mysubmit.onclick = function () {
-  if (mycheckbox.checked) {
-    if (my1.checked) {
-      myp.textContent = "you selected 1";
-    } else if (my2.checked) {
-      myp.textContent = "you selected 2";
-    } else if (my3.checked) {
-      myp.textContent = "you selected 3";
-    } else {
-      myp.textContent = "select radio button ";
-    }
-  } else {
-    myp.textContent = "select checkox toggle ";
-  }
-};
+document.getElementById("button").addEventListener("click", (evt) => {
+  username.innerText = text.innerText;
+});
