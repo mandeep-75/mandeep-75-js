ccb84f7283a41549a4067f6fafe4e9f563c2faf9
Mandeep-Singh <mandeep.dev1309@gmail.com>
2024-07-12 16:38:36 +0530

    zed-test

--- new/index.js
+++ new/index.js
@@ -4,9 +4,19 @@ class Animal {
     this.sound = sound;
     this.age = age;
   }
+  set name(newname) {
+    if (typeof newname === "string" && newname.length > 0) {
+      this._name = newname;
+    } else {
+      throw new Error("Invalid name");
+    }
+  }
+  get name() {
+    return this._name;
+  }
   greet() {
     console.log(
-      `${this.name} says ${this.sound} and is ${this.age} years old. ${this.breed}`
+      `${this.name} says ${this.sound} and is ${this.age} years old. ${this.breed}`,
     );
   }
 }
@@ -21,3 +31,4 @@ const jkimal = new Animal("karan", "cat", 34234);
 const karan = new Dog("karan", "dog", 32, "libera");
 
 jkimal.greet();
+class mali {}
