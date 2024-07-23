85f6fbd06de02788622ab48845a3fe53ae02f7db
Mandeep-Singh <mandeep.dev1309@gmail.com>
2024-07-23 18:11:03 +0530

    evnt

--- inc dec/index.js
+++ inc dec/index.js
@@ -1,9 +1,9 @@
 let display = document.getElementById("display");
 
-document.getElementById("inc").addEventListener("click", (evt) => {
+document.getElementById("inc").addEventListener("click", () => {
   display.innerText = Number(display.innerText) + 1;
 });
-document.getElementById("dec").addEventListener("click", (evt) => {
+document.getElementById("dec").addEventListener("click", () => {
   display.innerText = Number(display.innerText) - 1;
 });
 document.getElementById("reset").addEventListener("click", () => {
